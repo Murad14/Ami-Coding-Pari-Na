18037fbc27cdccb306d44962e106ef5c54b1598a
Murad <49407545+Murad14@users.noreply.github.com>
2023-08-14 13:38:22 +0600

    register done

--- kicuparina/users/urls.py
+++ kicuparina/users/urls.py
@@ -7,4 +7,6 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('login/', views.user_login, name='login'),  # type: ignore
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+    path('register/', views.register, name='register'),  # type: ignore
 ]
