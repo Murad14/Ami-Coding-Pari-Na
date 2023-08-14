3e6b1db64e6d23222198492d3106c8ddd1aa4319
Murad <49407545+Murad14@users.noreply.github.com>
2023-08-14 12:54:40 +0600

    modified

--- kicuparina/users/urls.py
+++ kicuparina/users/urls.py
@@ -1,10 +1,10 @@
 from re import template
 from django.urls import path
 from .import views
-from django.contrib.auth import views as auth_view 
+from django.contrib.auth import views as auth_view
 
 
 urlpatterns = [
-   path('login/', views.user_login, name='login'), 
-   path('logout/', auth_view.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
+    path('login/', views.user_login, name='login'),  # type: ignore
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
--- kicuparina/users/views.py
+++ kicuparina/users/views.py
@@ -1,26 +1,26 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate, login
 from .forms import LoginForm
-
 
 
 # Create your views here.
 
-def user_login(request:HttpRequest):
-    #login validation
+def user_login(request):
+    # login validation
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
             user = authenticate(
-                request,username = data['username'], password = data['password'])
+                request, username=data['username'], password=data['password'])
             if user is not None:
-                login(request,user)
+                login(request, user)
                 return HttpResponse("done")
             else:
                 return HttpResponse("Invalid credentials")
-        
+
     else:
-        form = LoginForm() #get the LoginForm from forms.py
-        return render(request, 'users/login.html',{'form':form}) #pass the form as a context
+        form = LoginForm()  # get the LoginForm from forms.py
+        # pass the form as a context
+        return render(request, 'users/login.html', {'form': form})
